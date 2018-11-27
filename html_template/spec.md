912121aa0258751b42d1ca85af14c0d72a4f8e60
Robin Edmunds <r.edmunds@live.co.uk>
2018-11-27 22:49:38 +0000

    Add files

---
+++ html_template/spec.md
@@ -0,0 +1,51 @@
+# Music Collector - Udacity Catalogue Project
+##### Robin Edmunds 2018
+
+## Description
+A web application for cataloguing and sharing personal music collections held on physical media, categorised by User.
+
+New users will have the option to create an app managed user profile, or use Google's or Facebook's OAuth2 to login.
+
+The application will provide API endpoints to allow for the integration of Music Collector data in third-party applications.
+
+## Database Design
+
+### Users
+- id
+- auth_type (google, facebook, native)
+- first_name
+- last_name
+- email
+- auth_token
+- description (of collection)
+- picture
+
+### Media
+- id
+- user_id (foreign key)
+- type (album, lp, single)
+- genre
+- medium (vinyl, cassette, cd, etc)
+- artist
+- title
+
+## Pages
+
+### READ
+1. Landing page (List of user collections on left, 10 latest entries right)
+1. Show collection page (Show list of collection entries)
+1. Show entry page (details of entry, media table info)
+
+### POST/PUT/DELETE
+1. Login page with google and facebook oauth
+1. Registration page (for creating native accounts)
+1. Edit collection description page (collection name the user's name)
+1. Delete collection with confirmation
+1. Add entry page
+1. Edit entry page
+1. Delete entry page with confirmation
+
+### API - JSON
+1. Show media item detail (public)
+1. List collections with description (public) - not essential
+1. List collection items (public) - not essential
---
+++ mcmodel.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+# import os
+# import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    auth_type = Column(String(2))
+    first_name = Column(String(32))
+    last_name = Column(String(32))
+    email = Column(String(64))
+    picture = Column(String(64))
+    auth_token = Column(String(64))
+    description = Column(String(256))
+
+
+class Media(Base):
+    __tablename__ = "media"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    type = Column(String(32))
+    genre = Column(String(64))
+    medium = Column(String(32))
+    artist = Column(String(64))
+    title = Column(String(128))
+
+
+
+
+
+
+#################
+
+# class Restaurant(Base):
+#     __tablename__ = 'restaurant'
+#
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
+#
+#     @property
+#     def serialize(self):
+#         # return object in serialisable format
+#         return {
+#             'id' : self.id,
+#             'name' : self.name,
+#         }
+#
+#
+# class MenuItem(Base):
+#     __tablename__ = 'menu_item'
+#
+#     name = Column(String(80), nullable = False)
+#     id = Column(Integer, primary_key = True)
+#     description = Column(String(250))
+#     price = Column(String(8))
+#     course = Column(String(250))
+#     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+#     restaurant = relationship(Restaurant)
+#
+#     @property
+#     def serialize(self):
+#         return {
+#             'name' : self.name,
+#             'description' : self.description,
+#             'id' : self.id,
+#             'price' : self.price,
+#             'course' : self.course,
+#         }
+
+
+
+engine = create_engine('sqlite:///mcollector.sqlite3')
+Base.metadata.create_all(engine)
